23d1df3d9c1b4ad20e0a4c72723313e4c132f32d
Ali Muksin <ali.muksin0510@gmail.com>
2026-04-26 20:41:05 +0700

    update: extract constant into separated folder

---
+++ src/constants/NavLinks.ts
@@ -0,0 +1,24 @@
+import type { Link } from '@/types';
+
+export const NavigationLinks: Link[] = [
+  {
+    id: '01',
+    label: 'WORK',
+    href: '/work',
+  },
+  {
+    id: '02',
+    label: 'BLOG',
+    href: '/blog',
+  },
+  {
+    id: '03',
+    label: 'ABOUT',
+    href: '/about',
+  },
+  {
+    id: '04',
+    label: 'HIRE ME',
+    href: '/hire me',
+  },
+];
